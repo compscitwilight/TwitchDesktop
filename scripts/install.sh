2d2882bfbb2f4c33b7a06a8e76c63d4e4b607633
devrusty <rguillory246@gmail.com>
2023-02-01 06:07:07 -0600

    new install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,30 @@
-if ! test npm
+#!/bin/sh
+# Written by https://github.com/jacob24068
+if [ -d "/usr/share/TwitchDesktop" ] 
 then
-    echo "FAIL: NPM is not installed";
+    sudo rm -rf /usr/share/TwitchDesktop
 fi
 
-if ! test electron
+sudo git clone --quiet https://github.com/devrusty/TwitchDesktop.git /usr/share/TwitchDesktop
+
+if [ -f /usr/share/applications/Twitch.desktop ]
 then
-    echo "FAIL: Electron is not installed.";
-    echo "Preparing installation..."
-    sudo npm install electron -g;
+    sudo rm /usr/share/applications/Twitch.desktop
 fi
 
-echo "Starting application"
-electron .
+sudo touch /usr/share/applications/Twitch.desktop
+echo "[Desktop Entry]" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "Name=Twitch" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "Path=/usr/share/TwitchDesktop/" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "Exec=npm run start" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "Terminal=false" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "Type=Application" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "Icon=/usr/share/TwitchDesktop/icon.png" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "StartupWMClass=Twitch" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "Comment=TWITCH TV WATCH STRAEMERS" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "GenericName=Streamer Watcher" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "StartupNotify=true" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "Categories=Entertainment" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+echo "MimeType=x-scheme-handler/twitch" | sudo tee -a /usr/share/applications/Twitch.desktop > /dev/null
+
+npm run install --prefix /usr/share/TwitchDesktop/ &> /dev/null
